a3a486a33f9c2b95b686b5e41f8429960b3cf58b
Gaurav Shetty <gauraviationphotography@gmail.com>
2025-07-13 16:21:28 +0530

    Email service

--- config/env.js
+++ config/env.js
@@ -8,5 +8,7 @@ export const { PORT, NODE_ENV,
      JWT_SECRET, JWT_EXPIRES_IN,
      ARCJET_KEY, ARCJET_ENV,
      QSTASH_URL, QSTASH_TOKEN,
-     SERVER_URL
+     SERVER_URL,
+     EMAIL_PASSWORD,
+     AccountEmail,
     } = process.env;
--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,6 +1,7 @@
 import { createRequire } from 'module';
 import dayjs from 'dayjs';
 import Subscription from '../models/subscription.model.js';
+import { sendReminderEmail } from '../utils/send-email.js';
 
 
 const require = createRequire(import.meta.url);
@@ -26,10 +27,10 @@ export const sendReminders = serve( async (context) => {
         const reminderDate = renewalDate.subtract(daysBefore, 'day');
 
         if(reminderDate.isAfter(dayjs())){
-            await sleepUntilReminder(context, `Reminder-${daysBefore} days before`, reminderDate);
+            await sleepUntilReminder(context, `${daysBefore} days before reminder`, reminderDate);
         }
 
-        await triggerReminder(context, `Reminder-${daysBefore} days before`);
+        await triggerReminder(context, `${daysBefore} days before reminder`, subscription);
     }
 })
 
@@ -44,8 +45,14 @@ const sleepUntilReminder = async (context, label, date) => {
     await context.sleepUntil(label, date.toDate())
 }
 
-const triggerReminder = async (context, label) => {
-    return await context.run(label, () => {
+const triggerReminder = async (context, label, subscription) => {
+    return await context.run(label, async () => {
         console.log(`Triggering ${label} reminder`)
+
+        await sendReminderEmail({
+            to: subscription.user.email,
+            type: label,
+            subscription
+        });
     })
 }
